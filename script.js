e21fef0bb8fe9745f1dddcfcf57957ad9261cead
anshinp <anshinp49@gmail.com>
2024-02-27 22:11:55 -0800

    Hey

---
+++ script.js
@@ -0,0 +1,21 @@
+// function add(a,b){
+//     return a+b
+   
+// }
+
+// let add = function(a,b){
+//     return a+b
+// }
+
+// let add =(a,b)=>{
+//     return a+b
+// }
+
+// add(1,2)
+// add(1,2)
+// console.log(add(1,2));
+
+console.log(add(1,2));
+function add(a,b){
+    return a+b
+}
